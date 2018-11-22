98f0d85caa7df80155f867deb0e660f57348bed2
Carvajal, Gianni <gianni.carvajal@etrade.com>
2018-11-22 09:06:12 -0600

    Addition of Beehive builders

--- src/singleton/Apiary.java
+++ src/singleton/Apiary.java
@@ -1,7 +1,12 @@
 package singleton;
 
+import builder.Beehive;
+
+import java.util.Hashtable;
+
 public class Apiary {
     private static Apiary _apiary = null;
+    private static Hashtable<Integer, Beehive> hives;
 
     private Apiary() {
 
