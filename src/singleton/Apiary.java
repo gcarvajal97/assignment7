d95979d0b3c7f2a8fc479469b46d9fe281949d6b
Carvajal, Gianni <gianni.carvajal@etrade.com>
2018-11-23 16:59:11 -0600

    Integration of Beehive builder with singleton apiary

--- src/singleton/Apiary.java
+++ src/singleton/Apiary.java
@@ -6,13 +6,39 @@ import java.util.Hashtable;
 
 public class Apiary {
     private static Apiary _apiary = null;
-    private static Hashtable<Integer, Beehive> hives;
+    private Hashtable<Integer, Beehive> hives;
+    private int numberOfHives = 0;
 
-    private Apiary() {
+    private Apiary() {}
 
-    }
     public static Apiary getApiary() {
         if (_apiary == null) _apiary = new Apiary();
         return _apiary;
+    }
+
+    public void addBeehive(int beeCount, String beeSpecies, int workers,
+                           int warriors, int kills, int hiveKills,
+                           String hiveStatus, int roomCount) {
+        Beehive newHive = new Beehive.Builder()
+                .setMaxBeeCount(beeCount)
+                .setSpecies(beeSpecies)
+                .setWorkerCount(workers)
+                .setWarriorCount(warriors)
+                .setKillCount(kills)
+                .setHiveKillCount(hiveKills)
+                .setTickCount(1)
+                .setHiveStatus(hiveStatus)
+                .setRoomCount(roomCount).build();
+        this.hives.put(numberOfHives, newHive);
+        this.numberOfHives++;
+    }
+
+    private Hashtable<Integer, Beehive> getHives() {
+        return hives;
+    }
+
+    public Beehive getSpecifiedHive(int key) {
+        Hashtable<Integer, Beehive> hives = this.getHives();
+        return hives.get(key);
     }
 }
